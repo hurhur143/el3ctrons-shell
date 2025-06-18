563b195a692a230d114216f8c8ffcfc19dad9416
el3ctron <thegamertnk@gmail.com>
2025-06-18 14:03:45 -0400

    Add files via upload

---
+++ build/CMakeFiles/shell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell.dir/main.c.o"
+  "CMakeFiles/shell.dir/main.c.o.d"
+  "libshell.dll.a"
+  "shell.exe"
+  "shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
